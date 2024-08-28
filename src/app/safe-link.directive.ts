bcf5ebe271e2ccc669a1d9555f4414e35de5ade9
Joshua Veal-Briscoe <josh.veal-briscoe@daugherty.com>
2024-08-28 15:26:16 -0500

    init

---
+++ src/app/safe-link.directive.ts
@@ -0,0 +1,32 @@
+import { Directive, ElementRef, inject, Input } from '@angular/core';
+import { LogDirective } from './log.directive';
+
+@Directive({
+  selector: 'a[appSafeLink]',
+  standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
+  hostDirectives: [LogDirective],
+})
+export class SafeLinkDirective {
+  @Input({ alias: 'appSafeLink' }) queryParam = 'myapp';
+  private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
+
+  constructor() {
+    console.log('SafeLinkDirective is active!');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm('Do you want to leave the app?');
+
+    if (wantsToLeave) {
+      const address = this.hostElementRef.nativeElement.href;
+      this.hostElementRef.nativeElement.href =
+        address + '?from=' + this.queryParam;
+      return;
+    }
+
+    event?.preventDefault();
+  }
+}
